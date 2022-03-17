56c0fa8ebf81a1dbf2c3df4b8044d04db045c65e
Jinson Cherian <jinche@nasdaq.com>
2022-03-18 01:35:09 +0530

    Read Configurations from Legacy Config files using Configuration Manager

---
+++ WebAPI/Controllers/HeaderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeaderController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public ActionResult<Dictionary<string, string>> GetAllHeaders()
+        {
+            Dictionary<string, string> requestHeaders =
+               new();
+            foreach (var header in Request.Headers)
+            {
+                requestHeaders.Add(header.Key, header.Value);
+            }
+            return requestHeaders;
+        }
+    }
+}
